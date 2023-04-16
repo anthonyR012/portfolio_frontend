c3994fa9cd3c8fac2747a5e6d11a4b6cd4599ae0
Anthony Rubio <rubionn27@gmail.com>
2023-04-16 13:01:13 -0500

    Merge prueba

--- public/assets/js/config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDuFuRXrTGpPubskNZCSIYiXJ02n49_q94",
-    authDomain: "solid-clone-334117.firebaseapp.com",
-    projectId: "solid-clone-334117",
-    storageBucket: "solid-clone-334117.appspot.com",
-    messagingSenderId: "815687967940",
-    appId: "1:815687967940:web:6f886da8a9ea1cc06e9591",
-  };
